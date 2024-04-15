3635cf0e09b6fad34c7ca9cfeb2e991c0783256f
Olli Saarenmaa <o.saarenmaa@gmail.com>
2024-04-15 05:07:08 +0300

    Location added

---
+++ weatherApp/app/src/main/java/com/example/weatherapp/composables/App.kt
@@ -0,0 +1,47 @@
+package com.example.weatherapp.composables
+
+import android.os.Build
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.lifecycle.viewmodel.compose.viewModel
+import android.Manifest
+import android.location.Location
+import androidx.compose.material3.Text
+import androidx.compose.runtime.State
+import androidx.compose.runtime.collectAsState
+import com.example.weatherapp.location.LocationViewModel
+
+@RequiresApi(Build.VERSION_CODES.S)
+@Composable
+fun App() {
+    val viewModel: LocationViewModel = viewModel()
+
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions(),
+        onResult = { permissions ->
+            // Check if all requested permissions have been granted
+            val allPermissionsGranted = permissions.entries.all { it.value }
+            if (allPermissionsGranted) {
+                // Start location updates through the ViewModel if permissions are granted
+                viewModel.startLocationUpdates()
+            }
+        }
+    )
+    LaunchedEffect(Unit) {
+        permissionLauncher.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ))
+    }
+    LocationDisplay(viewModel.location.collectAsState())
+}
+
+@Composable
+fun LocationDisplay(location: State<Location?>) {
+    Text(text = location.value?.let { loc ->
+        "Lat: ${loc.latitude}, Lon: ${loc.longitude}"
+    } ?: "Location not available")
+}
---
+++ weatherApp/app/src/main/java/com/example/weatherapp/location/LocationViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.weatherapp.location
+
+import android.app.Application
+import android.location.Location
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.AndroidViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+
+class LocationViewModel(application: Application) : AndroidViewModel(application) {
+    private val locationRepository = LocationRepository(application)
+
+    private val _location = MutableStateFlow<Location?>(null)
+    val location : StateFlow<Location?> = _location.asStateFlow()
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    fun startLocationUpdates() {
+        locationRepository.startLocationUpdates { location ->
+            _location.value = location
+        }
+    }
+}
